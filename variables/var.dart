4e152f43ccd917c7b194c4fb0e9c2c7771adf23d
Nabil <mahmud.stu20192@juniv.edu>
2023-03-05 12:25:21 +0600

    var.dart underwent changes

--- variables/var.dart
+++ variables/var.dart
@@ -1,8 +1,14 @@
 void main() {
-  var name = 'Bob';
-  // name is a variable
+  var first_name = 'Bob';
+// "name" is a variable
 // It stores refernce like any other variable.
 //Here it refers to a String object.
 //The value of that String object is "Bob".
-  print(name);
+//N.B. The type of the "name" variable is inferred to be "String".
+
+//===================================================================
+// type of s variable can be specified.
+  String last_name = 'Kennedy';
+
+  print(first_name + " " + last_name);
 }
