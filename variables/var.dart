cd8ba74b7db83a197700661c740f05c4f20cc77c
Nabil <mahmud.stu20192@juniv.edu>
2023-03-05 11:59:54 +0600

    Created 'variables' folder & var.dart file

---
+++ variables/var.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = 'Bob';
+  // name is a variable
+// It stores refernce like any other variable.
+//Here it refers to a String object.
+//The value of that String object is "Bob".
+  print(name);
+}
